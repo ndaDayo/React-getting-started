5b221843531b2a1e946980b3740693daa20cb4b9
hayato sugawara <hayato711@gmail.com>
2021-08-08 15:15:03 +0900

    list key01

--- src/index.js
+++ src/index.js
@@ -3,38 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-class Clock extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {date: new Date()};
-    }
-
-    componentDidMount() {
-        this.timerID = setInterval(
-            () => this.tick(),
-            1000
-        )
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.timerID)
-    }
-
-    tick() {
-        this.setState({
-            date: new Date()
-        })
-    }
-
-    render() {
-        return (
-            <p>It is {this.state.date.toLocaleTimeString()}</p>
-        )
-    }
-}
+const numbers = [1, 2, 3, 4, 5];
+const listItems = numbers.map((number) =>
+    <p>{number}</p>
+)
 
 ReactDOM.render(
-    <Clock/>,
+    listItems,
     document.getElementById('root')
 );
 // If you want to start measuring performance in your app, pass a function
